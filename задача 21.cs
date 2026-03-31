dcd13c051e1d18808ba7b69748b9fb0d57bf07f8
ladd2006 <diasvlad3@gmail.com>
2026-03-31 22:46:34 +0300

    Add files via upload

---
+++ задача 21.cs
@@ -0,0 +1,276 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static задача_21.Program;
+
+namespace задача_21
+{
+    internal class Program
+    {
+        public class Entry<T,P>
+        {
+            public T id;
+            public P value;
+
+            public Entry(T id, P value)
+            {
+                this.id = id;
+                this.value = value;
+            }
+        }
+        public class MyHashMap<T,P>
+        {
+            public LinkedList<Entry<T,P>>[] table;
+            public int size;
+            public int count;
+            public float loadFactor;
+
+            public MyHashMap()
+            {
+                table = new LinkedList<Entry<T,P>>[16];
+                size = 16;
+                count = 0;
+                loadFactor = 0.75f;
+            }
+            public MyHashMap(int initialCapacity)
+            {
+                table = new LinkedList<Entry<T,P>>[initialCapacity];
+                size = initialCapacity;
+                count = 0;
+                loadFactor = 0.75f;
+            }
+            public MyHashMap(int initialCapacity, float loadFactor)
+            {
+                table = new LinkedList<Entry<T,P>>[initialCapacity];
+                size = initialCapacity;
+                count = 0;
+                this.loadFactor = loadFactor;
+            }
+            public void recize()
+            {
+                var old = table;
+                int newsize = size * 2;
+
+                table = new LinkedList<Entry<T, P>>[newsize];
+                size = newsize;
+                count = 0;
+
+                foreach(var buck in old)
+                {
+                    if( buck != null )
+                    {
+                        foreach(var entry in buck)
+                        {
+                            put(entry.id, entry.value);
+                        }
+                    }
+                }
+
+
+            }
+            public int hashCode(T key)
+            {
+                return Math.Abs(key.GetHashCode()) % size;
+            }
+            public void clear()
+            {
+                table = new LinkedList<Entry<T,P>>[size];
+                count = 0;
+            }
+            public bool containsKey(object key)
+            {
+                if(key == null)
+                    return false;
+                T tKEY = (T)key;
+
+                int hashkey = hashCode(tKEY);
+                if (table[hashkey] == null)
+                    return false;
+
+                foreach(var entry in table[hashkey])
+                {
+                    if(entry.id.Equals(tKEY))
+                    {
+                        return true;
+                    }
+                }
+                return false;
+            }
+            public bool containsValue(object value)
+            {
+                if(value == null)
+                    return false;
+               for(int i = 0;i < size; i++)
+                {
+                    if (table[i] != null)
+                    {
+                        foreach (var valuee in table[i])
+                        {
+                            if (valuee.value.Equals(value))
+                            {
+                                return true;
+                            }
+                        }
+                    }
+                }
+                return false;
+            }
+            public List<Entry<T,P>> entrySet()
+            {
+                List< Entry<T,P>> res = new List<Entry<T, P>>();
+
+                for (int i = 0; i < size; i++)
+                {
+                    if(table[i] != null)
+                    {
+                        foreach(var entry in table[i])
+                        {
+                            res.Add(entry);
+                        }
+                    }
+
+                }
+                return res;
+            }
+            public P get(object key)
+            {
+                if (key == null)
+                    return default(P);
+
+                T tKey = (T)key;
+                int hashKey = hashCode(tKey);
+
+                if (table[hashKey] != null)
+                {
+                   foreach(var entry in table[hashKey])
+                    {
+                        if(entry.id.Equals(tKey))
+                        {
+                            return entry.value;
+                        }
+                    }
+                }
+                return default(P);
+            }
+            public bool isEmpty()
+            {
+                if(count == 0) return false;
+                return true;
+
+            }
+            public List<T> keySet()
+            {
+                List<T> res = new List<T>();
+
+                for( int i = 0;i < size;i++)
+                {
+                    if( table[i] != null)
+                    {
+                        foreach(var entry in table[i])
+                        {
+                            res.Add(entry.id);
+                        }
+                    }
+                }
+                return res;
+            }
+            public bool put(T key, P value)
+            {
+                if(key == null)
+                return false;
+               
+                             
+                if (count >= size * loadFactor)
+                {
+                    recize();
+                }
+
+                int hashKey = hashCode(key);
+                if (table[hashKey] == null)
+                {
+                    table[hashKey] = new LinkedList<Entry<T, P>>();
+                }
+
+                foreach (var entry in table[hashKey])
+                {
+                    if( entry.id.Equals(key))
+                    {
+                        entry.value = value;
+                        return true;
+                    }
+                }
+                table[hashKey].AddLast(new Entry<T, P>(key,value));
+                count++;
+                return true;
+                                      
+            }
+            public bool remove(object key)
+            {
+                if( key == null ) return false; 
+
+                T tkey = (T)key;    
+
+                int hashKey = hashCode(tkey);
+                if (table[hashKey]  != null)
+                {
+                    foreach(var entry in  table[hashKey])
+                    {
+                        if(entry.id.Equals(tkey))
+                        {
+                            table[hashKey].Remove(entry);
+                            count--;
+                            return true;
+                        }
+                    }
+                }
+                return false;
+            }
+            public int Size()
+            {
+                return count;
+            }
+        }
+        static void Main(string[] args)
+        {
+            MyHashMap<string, int> map = new MyHashMap<string, int>();
+
+            map.put("Анна", 25);
+            map.put("Борис", 30);
+            map.put("Виктор", 35);
+
+            Console.WriteLine($"Размер: {map.Size()}");
+            Console.WriteLine($"Анна: {map.get("Анна")}");
+            Console.WriteLine($"Борис: {map.get("Борис")}");
+            Console.WriteLine($"Есть Анна: {map.containsKey("Анна")}");
+            Console.WriteLine($"Есть Петр: {map.containsKey("Петр")}");
+            Console.WriteLine($"Есть значение 25: {map.containsValue(25)}");
+
+            map.put("Анна", 26);
+            Console.WriteLine($"Анна после обновления: {map.get("Анна")}");
+
+            Console.WriteLine("\nВсе пары:");
+            foreach (var entry in map.entrySet())
+            {
+                Console.WriteLine($"  {entry.id} -> {entry.value}");
+            }
+
+            Console.WriteLine($"\nУдаляем Бориса: {map.remove("Борис")}");
+            Console.WriteLine($"Размер после удаления: {map.Size()}");
+
+            Console.WriteLine($"\nТаблица пуста: {map.isEmpty()}");
+
+            map.clear();
+            Console.WriteLine($"Размер после очистки: {map.Size()}");
+            Console.WriteLine($"Таблица пуста: {map.isEmpty()}");
+
+            MyHashMap<int, string> map2 = new MyHashMap<int, string>(4, 0.75f);
+            for (int i = 0; i < 10; i++)
+            {
+                map2.put(i, $"Элемент{i}");
+            }
+            Console.WriteLine($"\nПосле 10 элементов: size={map2.size}, count={map2.count}");
+        }
+    }
+}
